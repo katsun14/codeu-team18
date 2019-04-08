152fad192224df56cec0603ca83c4c70a73c4835
katsun14 <katherinesun14@gmail.com>
2019-04-08 00:16:11 -0400

    Created events page, a forum where users can discuss the Olympic events that interest them.

---
+++ src/main/webapp/js/events-loader.js
@@ -0,0 +1,53 @@
+// Fetch messages and add them to the page.
+function fetchMessages() {
+  const url = '/events';
+  fetch(url).then((response) => {
+    return response.json();
+  }).then((messages) => {
+    const messageContainer = document.getElementById('message-container');
+    if (messages.length == 0) {
+      messageContainer.innerHTML = '<p>There are no posts yet.</p>';
+    }
+    else {
+      messageContainer.innerHTML = '';
+    }
+    messages.forEach((message) => {
+      const messageDiv = buildMessageDiv(message);
+      messageContainer.appendChild(messageDiv);
+    });
+  });
+}
+
+function buildMessageDiv(message) {
+  const usernameDiv = document.createElement('div');
+  usernameDiv.classList.add("email-header");
+  usernameDiv.appendChild(document.createTextNode(message.user));
+
+  const timeDiv = document.createElement('div');
+  timeDiv.classList.add('info-header');
+  timeDiv.appendChild(document.createTextNode(new Date(message.timestamp)));
+
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('message-header');
+  headerDiv.appendChild(usernameDiv);
+  headerDiv.appendChild(timeDiv);
+
+  // const bodyDiv = document.createElement('div');
+  // bodyDiv.classList.add('message-body');
+  // bodyDiv.appendChild(document.createTextNode(message.text));
+  const bodyDiv = document.createElement('div');
+  bodyDiv.classList.add('message-body');
+  bodyDiv.innerHTML = message.text;
+
+  const messageDiv = document.createElement('div');
+  messageDiv.classList.add("message-div");
+  messageDiv.appendChild(headerDiv);
+  messageDiv.appendChild(bodyDiv);
+
+  return messageDiv;
+}
+
+// Fetch data and populate the UI of the page.
+function buildUI() {
+  fetchMessages();
+}
